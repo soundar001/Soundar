47644715653239bced9fd7f9cd8e595d956098ad
soundar001 <soundar813@gmail.com>
2018-08-20 14:11:17 +0530

    Update and rename Main.java to Main1.java

---
+++ Main1.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+class Main1
+{
+public static void main(String[] args)
+{
+Scanner s = new Scanner(System.in);
+char a;
+if(a==a||a==e||a==i||a==o||a==u)
+{
+System.out.println("Vowel");
+}
+else if(a==
