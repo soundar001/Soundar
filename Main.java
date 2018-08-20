431b881f4cb005fdb67b24b3e1b6a99964d83f5e
soundar001 <soundar813@gmail.com>
2018-08-20 14:04:09 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+class Main
+{
+public static void main(String args[])
+{
+
+Scanner sc= new Scanner(System.in);
+int a=sc.nextInt();
+if(a/2==0)
+{
+System.out.println("Even");
+}
+else
+{
+System.out.println("Odd");
+}
+}
+}
