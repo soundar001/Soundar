77338c8730a39866502fdeb39665d81f5133f0f6
soundar001 <soundar813@gmail.com>
2018-08-27 13:45:00 +0530

    Update Main1.java

--- Main1.java
+++ Main1.java
@@ -4,10 +4,14 @@ class Main1
 {
 public static void main(String[] args)
 {
-Scanner s = new Scanner(System.in);
-char a;
+char s='a';
 if(a==a||a==e||a==i||a==o||a==u)
 {
 System.out.println("Vowel");
 }
-else if(a==
+else 
+{
+  System.out.println("Consonant");
+}
+}
+}
