0325a60b2c413d2f9ba5418944e7ddf8488cfe25
soundar001 <soundar813@gmail.com>
2018-09-03 12:34:01 +0530

    Create Nnumber.java

---
+++ Nnumber.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+class Nnumber
+{
+public static void main(String[] args)
+{
+int a,b=0;
+Scanner sc=new Scanner(System.in);
+a=sc.NextInt();
+for(int i=1;i<=a;i++)
+{
+b=b+i;
+System.out.println(i);
+}
+}
+}
