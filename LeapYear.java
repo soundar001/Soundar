46407013f4465111b310068e515e6d3ee4db87f8
soundar001 <soundar813@gmail.com>
2019-01-25 11:35:11 +0530

    Update LeapYear.java

--- LeapYear.java
+++ LeapYear.java
@@ -4,9 +4,8 @@ class LeapYear
 {
 public Static void main(String args[])
 {
-int a;
 Scanner sc= new Scanner(System.in)
-if(a%4==0)
+if(sc%4==0)
   system.out.println("yes")
 else
   system.out.println("no")
