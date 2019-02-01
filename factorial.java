f2b881c35ffb7e12cb7ccc7d12acdb94f523d581
soundar001 <soundar813@gmail.com>
2019-02-01 11:50:56 +0530

    Create factorial.java

---
+++ factorial.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class Factorial
+{
+  public stativ void main(String args[])
+  {
+     int a,b,fact=1;
+     System.out.println("Enter the number");
+     Scanner sc = new Scanner(System.in);
+     a = in.nextInt();
+     if(n<0)
+     {
+      System.out.println("Number shuold be non-negative");
+     }
+     else
+     {
+         for (b = 1;b<=a;c++)
+            {
+              fact=fact*b;
+             }
+          System.out.println("Factorial of "+n+" is ="+fact);
+       }
+    }
+   }
