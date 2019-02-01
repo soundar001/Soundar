a4871ba24c9d3fa715dfed73df3f2382b97d7059
soundar001 <soundar813@gmail.com>
2019-02-01 12:44:59 +0530

    Create dublicateNumber.java

---
+++ dublicateNumber.java
@@ -0,0 +1,21 @@
+class dublicateNumber
+{
+int[] numbers = new int[10];
+Scanner sc = new Scanner(System.in);
+boolean b = true;
+int temp = -1;
+  public static void main(String args[])
+  {
+    System.out.println("Enter the numbers");
+    for(int i =0;i < numbers.length;i++)
+    {
+      numbers [i]=inn.nextInt();
+    }
+    System.out.println("Reapted numbers are");
+    for(int i=0; i<numbers.lenth;i++
+    {
+    if(!b)
+    {
+    System.out.println(temp +" ");
+    }
+    b=true;
