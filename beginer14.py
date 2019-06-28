b43b72955b7f81b662c2c9f17b2632c2b28a1529
soundar001 <soundar813@gmail.com>
2019-06-28 17:20:28 +0530

    Update beginer14.py

--- beginer14.py
+++ beginer14.py
@@ -1,9 +1,9 @@
 a,b=map(int,input().split())
-if(a%2==0):
+if (a%2==0):
   for i in range (a,b):
-    if(i%2==1):
-      print(i)
+     if(i%2==1):
+         print(i)
 else:
-  for i in range (a+1,b):
-    if(i%2==1):
-      print(i)
+    for i in range (a+1,b):
+        if(i%2==1):
+            print(i)
